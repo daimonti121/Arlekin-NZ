251fe5f81d7c72a532ccca063c3f2f1a3f4e1bea
daimonti121 <damios.1812@gmail.com>
2021-12-14 22:42:08 +0300

    2

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ window.HUB_EVENTS={ASSET_ADDED:"ASSET_ADDED",ASSET_DELETED:"ASSET_DELETED",ASSET
 
 var colors = ["#3a1149", "#f30000", "#24177c", "#ec710b",
              "#3a1149", "#f30000", "#24177c", "#ec710b"];
-var prizes = ["TRY AGAIN", " 100% up to 300 NZD", "50 FS", "100 FS", "NO WIN", "  50% up to   375 NZD", "200 FS", "  75% up to   450 NZD"];
+var prizes = ["TRY AGAIN", "  100% up to   300 NZD", "50 FS", "100 FS", "NO WIN", "  50% up to   375 NZD", "200 FS", "  75% up to   450 NZD"];
 
 var startAngle = 0*Math.PI/180;
 var arc = (2 * Math.PI) / colors.length;
